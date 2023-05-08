793c262da1239208b527eaa827e1c49ff613e4c2
sandro888 <36017398+sandro888@users.noreply.github.com>
2023-05-09 02:34:13 +0400

    e

---
+++ frontend/src/data.js
@@ -0,0 +1,64 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Avengers',
+      category: 'fantasy',
+      image: '/images/1.jpg',
+      price: 60,
+      brand: ' disney',
+      description: 'The most frustrating issue here is one of narrative focus—Stateless engages the refugee crises with some extremely tedious optics.',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Bohemian Rhapsody',
+      category: 'drama',
+      image: '/images/2.jpg',
+      price: 50,
+      description: 'Queen is dope.',
+
+      brand: ' disney',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Amelie',
+      category: 'paramount',
+      image: '/images/3.jpg',
+      price: 70,
+      description: 'Amelie" is a delicious pastry of a movie, a lighthearted fantasy in which a winsome heroine overcomes a sad childhood and grows up to bring cheer to the needful and joy to herself. You see it, and later when you think about it, you smile.',
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    }, {
+      _id: '4',
+      name: '1917',
+      category: 'action',
+      image: '/images/4.jpg',
+      price: 70,
+      brand: ' paramount',
+      description: 'Crazy athmosphere',
+
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    },
+     {
+      _id: '5',
+      name: 'Iron Giant',
+      category: 'animation',
+      image: '/images/5.jpg',
+      price: 25,
+      brand: ' paramount',
+      description: 'Love',
+      rating: 4.9,
+      numReviews: 4,
+      countInStock: 12,
+    },
+  ]
+}
